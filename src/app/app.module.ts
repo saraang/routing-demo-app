7134b0e96e1bbec0f19286645b660f986ff2effa
saraang <saraangsk@gmail.com>
2019-07-29 15:40:14 +0530

    Routing Implemented Successfully...!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ErrorComponent } from './error/error.component';
@@ -12,7 +11,7 @@ import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, ContactComponent, AboutusComponent, ErrorComponent, HomeComponent ],
+  declarations: [ AppComponent, ContactComponent, AboutusComponent, ErrorComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
